ba0e7b48a09927b47f1ca160ad961b62655f40d2
Rick <141774554+Sttorm1@users.noreply.github.com>
2023-08-18 04:48:41 -0300

    Add files via upload
    Comecei recentemente a estudar C#, estarei atualizando os codigos aqui conforme forem sendo feitos

---
+++ C# Codes/CalculadoraCsharp.cs
@@ -0,0 +1,14 @@
+using System
+
+namespace calculadora
+{
+
+    class program
+{
+
+    static void Main(string[] args)
+{
+        console.writeLine("calculadora simples");
+
+        console.write("digite o primeiro nmero:");
+        double numero1 = convert.todouble(console.readline())
